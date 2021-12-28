3e267e161c7e7193bdd024e9b228f9eb1ddb4e16
choniyuazwan <choniyuazwan@gmail.com>
2021-12-28 22:10:17 +0700

    feat: create model user conversation message

---
+++ db/migrate/20211228145828_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.string :content
+      t.references :conversation, null: false, foreign_key: true
+      t.boolean :is_read
+
+      t.timestamps
+    end
+  end
+end
